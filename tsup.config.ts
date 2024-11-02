2af46435d888ab4d0a6502ddc4395fd858ec783b
Fatih Aygün <cyco130@gmail.com>
2024-11-02 08:01:28 +0000

    chore: update deps and add ESM entry

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from "tsup";
 export default defineConfig([
 	{
 		entry: ["src/index.ts", "src/cli.ts"],
+		format: ["esm"],
+		target: "node18",
+		dts: {
+			entry: "src/index.ts",
+		},
+	},
+	{
+		entry: ["src/index.ts"],
 		format: ["cjs"],
 		target: "node18",
 		dts: {
